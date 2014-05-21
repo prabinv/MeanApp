0819d11aa63e21e0731b0130a6aa3e6e279efa44
Prabin Varma <prabinv@gmail.com>
2014-05-21 14:27:32 -0500

    UI Design

---
+++ public/app/main/mvMainCtrl.js
@@ -0,0 +1,89 @@
+angular.module('app').controller('nvMainCtrl', ['$scope', function ($scope) {
+	$scope.courses = [
+    {
+        'name': 'deserunt incididunt culpa nulla',
+        'featured': false,
+        'published': new Date('2014-01-08')
+    },
+    {
+        'name': 'quis sit sit nostrud',
+        'featured': true,
+        'published': new Date('2014-02-07')
+    },
+    {
+        'name': 'adipisicing ipsum nulla irure',
+        'featured': true,
+        'published': new Date('2014-03-30')
+    },
+    {
+        'name': 'sunt amet excepteur veniam',
+        'featured': false,
+        'published': new Date('2014-04-29')
+    },
+    {
+        'name': 'nisi commodo sint veniam',
+        'featured': true,
+        'published': new Date('2014-01-07')
+    },
+    {
+        'name': 'fugiat commodo quis quis',
+        'featured': true,
+        'published': new Date('2014-03-21')
+    },
+    {
+        'name': 'esse eiusmod laboris duis',
+        'featured': true,
+        'published': new Date('2014-02-03')
+    },
+    {
+        'name': 'dolore duis aliquip cillum',
+        'featured': false,
+        'published': new Date('2014-01-28')
+    },
+    {
+        'name': 'anim elit irure do',
+        'featured': true,
+        'published': new Date('2014-03-02')
+    },
+    {
+        'name': 'commodo consectetur officia ex',
+        'featured': true,
+        'published': new Date('2014-04-06')
+    },
+    {
+        'name': 'culpa irure nisi duis',
+        'featured': true,
+        'published': new Date('2014-02-09')
+    },
+    {
+        'name': 'elit proident exercitation aute',
+        'featured': true,
+        'published': new Date('2014-03-24')
+    },
+	{
+        'name': 'excepteur est quis eiusmod',
+        'featured': false,
+        'published': new Date('2014-04-12')
+    },
+    {
+        'name': 'ea laborum duis deserunt',
+        'featured': false,
+        'published': new Date('2014-01-09')
+    },
+    {
+        'name': 'mollit duis culpa incididunt',
+        'featured': true,
+        'published': new Date('2014-02-25')
+    },
+    {
+        'name': 'commodo irure ipsum irure',
+        'featured': false,
+        'published': new Date('2014-02-06')
+    },
+    {
+        'name': 'proident quis ad dolor',
+        'featured': true,
+        'published': new Date('2014-04-23')
+    }
+];
+}]);
