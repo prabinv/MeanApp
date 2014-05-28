5fccc3fa14ea1f3f7f832ad373fa30e969403151
Prabin Varma <prabinv@gmail.com>
2014-05-28 13:11:39 -0500

    Persisting authentication

--- server.js
+++ server.js
@@ -1,10 +1,8 @@
 var express = require('express'),
-	passport = require('passport'),
-	LocalStrategy = require('passport-local').Strategy,
+	passportConfig = require('./server/config/passport'),
 	configureExpress = require('./server/config/express'),
 	configSettings = require('./server/config/config'),
 	mongooseConfig = require('./server/config/mongoose'),
-	mongoose = require('mongoose'),
 	routesConfig = require('./server/config/routes');
 
 var env = process.env.NODE_ENV || 'development';
@@ -17,37 +15,7 @@ configureExpress(app, config);
 
 mongooseConfig(config);
 
-var User = mongoose.model('User');
-passport.use(new LocalStrategy(
-	function(username, password, done) {
-		User.findOne({userName: username}).exec(function(err, user) {
-			console.log('user ' , user);
-			if (user && user.authenticate(password)) {
-				return done(null, user);
-			} else {
-				return done(null, false);
-			}
-
-		});
-	}
-));
-
-passport.serializeUser(function(user, done) {
-	if(user) {
-		done(null, user._id);
-	}
-});
-
-passport.deserializeUser(function(id, done) {
-	User.findOne({_id: id}).exec(function(err, user) {
-		if (user) {
-			return done(null, user);
-		} else {
-			return done(null, false);
-		}
-
-	});
-});
+passportConfig();
 
 routesConfig(app);
 
